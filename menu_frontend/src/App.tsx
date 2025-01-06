62536b818c2c8ca85861888d7f387797bcc0fbf9
lauramesquitabruel <lauramesquitabruel@gmail.com>
2025-01-06 11:03:03 -0300

    integração das partes

--- menu_frontend/src/App.tsx
+++ menu_frontend/src/App.tsx
@@ -1,9 +1,16 @@
+import { useState } from 'react';
 import './App.css'
 import { Card } from './components/card/card';
 import { useFoodData } from './hooks/useFoodData';
+import { CreateModal } from './components/create-modal/createModal';
 
 function App() {
   const {data} = useFoodData();
+  const [isModalOpen, setIsModalOpen]= useState(false);
+
+  const handleOpenModal = () => {
+    setIsModalOpen(prev => !prev);
+  }
 
   return (
     <div className='container'>
@@ -16,6 +23,8 @@ function App() {
           image={foodData.image} 
         />)}
       </div>
+      {isModalOpen &&<CreateModal closeModal={handleOpenModal}></CreateModal>}
+      <button onClick={handleOpenModal}>Cadastrar</button>
     </div>
   )
 }
--- menu_frontend/src/components/create-modal/createModal.tsx
+++ menu_frontend/src/components/create-modal/createModal.tsx
@@ -1,4 +1,7 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import { useFoodDataMutate } from "../../hooks/useFoodDataMutate";
+import { FoodData } from "../../interface/FoodData";
+import "./modal.css";
 
 interface InputProps {
     label: string, 
@@ -14,13 +17,32 @@ const Input = ({label, value, updateValue}: InputProps) => {
     )
 }
 
-export function CreateModal(){
+interface ModalProps {
+    closeModal(): void
+}
+
+export function CreateModal({closeModal}: ModalProps){
     const [title, setTitle] = useState("");
     const [price, setPrice] = useState(0);
     const [image, setImage] = useState("");
+    const {mutate, isSuccess} = useFoodDataMutate();
+
+    const sumbit = () => {
+        const foodData: FoodData = {
+            title,
+            price,
+            image
+        }
+        mutate(foodData);
+    }
+
+    useEffect(() => {
+        if(!isSuccess) return
+        closeModal();
+    }, [isSuccess]);
 
     return(
-    <div className="modal-overflow">
+    <div className="modal-overlay">
         <div className="modal-body">
             <h2>Cadastre um novo item no cardápio</h2>
             <form className="input-container">
@@ -28,6 +50,7 @@ export function CreateModal(){
                 <Input label="price" value={price} updateValue={setPrice}></Input>
                 <Input label="image" value={image} updateValue={setImage}></Input>
             </form>
+            <button onClick={sumbit} className="btn-secondary">Enviar</button>
         </div>
     </div>)
 }
